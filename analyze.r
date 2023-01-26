f90a3d6e9cd4755dc5e5a6d1d2505e3b3d83d3aa
kwpap <kwpaptypetomaniac97@gmail.com>
2023-01-26 13:37:24 +0200

    one country modification

--- analyze.r
+++ analyze.r
@@ -5,12 +5,11 @@ source("find_slopes.R")
 
 
 find_the_best_combo <- function(){
-  # weights <- c(1,1,1,1,1,1,1,1)
-  weights <- c(500, 500, 500, 500, 500, 500, 500, 500)
+  weights <- rep(1,8)
   r_squared <- summary(find_slopes()$linear)$r.squared
-  step <- 10
+  step <- 1
   low <- 0
-  high <- 10000
+  high <- 10
   for (i in 1:40){
     # index <- i %% 8 +1
     index <- sample(1:8, 1)
@@ -47,4 +46,41 @@ find_the_best_combo <- function(){
 
 # create an average allocation comparing to the median of all.
 
-new_allocation <- function()
+find_the_best_combo_with_one <- function(){
+  weights <- rep(1,8)
+  r_squared <- summary(find_slopes_with_one_country_with_weights(weights = weights)$linear)$r.squared
+  step <- 0.1
+  low <- 0
+  high <- 10
+  for (i in 1:40){
+    # index <- i %% 8 +1
+    index <- sample(1:8, 1)
+    worth_doing_it <- TRUE
+    while(worth_doing_it){
+      lowered <- 0
+      raised <- 0
+      if (weights[index]>low+step){
+        weights[index] <- weights[index] - step
+        lowered <- summary(find_slopes_with_one_country_with_weights(weights = weights)$linear)$r.squared
+        weights[index] <- weights[index] + step
+      }
+      if (weights[index]<high){
+        weights[index] <- weights[index] + step
+        raised <- summary(find_slopes_with_one_country_with_weights(weights = weights)$linear)$r.squared
+        weights[index] <- weights[index] - step
+      }
+      if (lowered > r_squared){
+        r_squared <- lowered
+        weights[index] <- weights[index] - step
+      } else if (raised > r_squared){
+        r_squared <- raised
+        weights[index] <- weights[index] + step
+      } else {
+        worth_doing_it <- FALSE
+      }
+    }
+  }
+  print(weights)
+  print(paste("Population: ", weights[1], "GDPpc: ", weights[2], "Inflation: ", weights[3], "Agriculture: ", weights[4], "Industry: ", weights[5], "Manufacturing: ", weights[6], "Total Energy Supply: ", weights[7], "Verified Emissions: ", weights[8]))
+  print(paste("R^2:", r_squared))
+}
--- find_slopes.r
+++ find_slopes.r
@@ -210,3 +210,110 @@ find_slopes_with_weights <- function(weights){
   return (find_slopes(weight_population = weights[1], weight_GDPpc = weights[2], weight_inflation = weights[3], weight_agriculture = weights[4], weight_industry = weights[5], weight_manufacturing = weights[6], weight_total_energy_supply = weights[7], weight_verified_emisions = weights[8]))
 }
 
+find_slopes_with_one_country <- function(year = 0, weight_population = 1, weight_GDPpc = 1, weight_inflation = 1, weight_agriculture = 1, weight_industry = 1, weight_manufacturing = 1, weight_total_energy_supply = 1, weight_verified_emisions = 1, country = "none"){
+  if(year != 0) {year_for_comparison <- year}
+  
+
+  #df_data <-read_data( TRUE, 2016,  TRUE,  TRUE,  TRUE,  TRUE,  TRUE,  TRUE, TRUE, TRUE, TRUE, TRUE, TRUE)
+  df_data <-read_data(year_for_comparison)
+  #df_free <- read_free(df_data$GEO, year = 2016 ,TRUE, TRUE)
+  df_free <- read_free(year_for_comparison)
+
+  if(country == "none"){ #find the median country with respect to the df_data and the values in it
+   ranking <- data.frame(country = df_data$GEO, value = 0)
+   for (i in 2:ncol(df_data)){
+    buffer <- data.frame(country = df_data$GEO, value = df_data[,i])
+    # print (colnames(df_data)[i])
+    buffer <- buffer[order(buffer$value),]
+    # print(buffer)
+    for (j in 1:nrow(buffer)){
+      # print(buffer[j,1])
+      ranking[which(buffer[j,1] == ranking$country ),2] <- ranking[which(buffer[j,1] == ranking$country ),2] + j
+    }
+   }
+    ranking <- ranking[order(ranking$value),]
+    # print(ranking)
+    country <- ranking[nrow(ranking)/2,1]
+    # print(paste("The median country is ", country, sep = ""))
+  }
+  country_index <- which(df_data$GEO == country)
+
+  text_s <- paste("df_all_compared_to_country_",year_for_comparison,(if (will_use_log) "_log" else ""),
+                (if(will_normalise) "_norm" else ""),
+                (if(will_use_total_energy_supply) "_tes" else ""),
+                (if(will_use_verified_emisions) "_ve" else ""),
+                (if(will_use_GDPpc) "_gdp" else ""),
+                (if(will_use_population) "_pop" else ""),
+                (if(will_use_inflation) "_inf" else ""),
+                (if(will_use_agriculture) "_agr" else ""),
+                (if(will_use_industry) "_ind" else ""),
+                (if(will_use_manufacturing) "_man" else ""),
+                ".csv", sep = "")
+
+  
+  # Create a 2 D dataframe with countries as rows and countries as columns and the value of the Eukleidian Distance between all the values the two countries
+  df_distance <- data.frame(matrix(NA, nrow = nrow(df_data) - 1, ncol = 3))
+  colnames(df_distance) <- c("GEO", "df_distance", "df_free_distance")
+  for (i in 1 : nrow(df_distance)) { #nolint
+  df_distance[i, 1] <- df_data[i, 1]
+      df_distance[i, 2] <- sqrt((df_data[i, 2] - df_data[country_index, 2])^2 * weight_total_energy_supply 
+      + (df_data[i, 3] - df_data[country_index, 3])^2 * weight_GDPpc
+      + (df_data[i, 4] - df_data[country_index, 4])^2 * weight_population
+      + (df_data[i, 5] - df_data[country_index, 5])^2 * weight_inflation
+      + (df_data[i, 6] - df_data[country_index, 6])^2 * weight_verified_emisions
+      + (df_data[i, 7] - df_data[country_index, 7])^2 * weight_agriculture
+      + (df_data[i, 8] - df_data[country_index, 8])^2 * weight_industry
+      + (df_data[i, 9] - df_data[country_index, 9])^2 * weight_manufacturing
+      )
+  }
+  
+  # save a file "distance_2015.tex" with the content of the dataframe df_distance
+  #<<results=tex>>
+   #   xtable(df_distance[,21:26], caption = "Distance between countries in 2015", label = "tab:distance_2015")
+  #@ 
+  
+  # Calculate Eukleidian distance of df_free_allocation and store them in df_actual_distance
+  country_index <- which(df_free$GEO == country)
+  for (i in 1 : nrow(df_distance)) { #nolint
+    df_distance[i, 3] <- sqrt((df_free[country_index, 2] - df_free[i, 2])^2 )
+  }
+  # Remove rows with NA and 0
+  df_distance <- df_distance[!is.na(df_distance$"df_distance"),]
+  df_distance <- df_distance[!is.na(df_distance$"df_free_distance"),]
+  df_distance <- df_distance[df_distance$"df_distance" != 0,]
+  df_distance <- df_distance[df_distance$"df_free_distance" != 0,]
+  
+  lm <- lm(df_distance$"df_free_distance" ~ df_distance$"df_distance")
+  summary(lm)
+  
+  #write that summary to a file
+  sink(paste("linear_regration_summary_for", text_s, ".txt"))
+  summary(lm)
+  sink()
+  # plot the regression line
+  plot(df_distance$"df_distance", df_distance$"df_free_distance", xlab = "Distance between countries in 2015", ylab = "Distance between countries in 2015 with free allocation", main = paste("Distance between countries in 2015 and in 2015 with free allocation", year_for_comparison))
+  abline(lm, col = "red")
+  return (list( data = df_distance, linear =  lm))
+}
+  
+find_slopes_with_one_country_with_weights <- function( year = 2015, name = "Germany", weights = c(1,1,1,1,1,1,1,1)){
+  if(year != 0) {year_for_comparison <- year}
+  df_1D <- find_slopes_with_one_country(year = year_for_comparison, weight_population = weights[1], weight_GDPpc = weights[2], weight_inflation = weights[3], weight_agriculture = weights[4], weight_industry = weights[5], weight_manufacturing = weights[6], weight_total_energy_supply = weights[7], weight_verified_emisions = weights[8])$data
+  lm <- find_slopes_with_one_country(year = year_for_comparison, weight_population = weights[1], weight_GDPpc = weights[2], weight_inflation = weights[3], weight_agriculture = weights[4], weight_industry = weights[5], weight_manufacturing = weights[6], weight_total_energy_supply = weights[7], weight_verified_emisions = weights[8])$linear
+  return (list( data = df_1D, linear =  lm))
+}
+
+create_graph_for_one <- function (year = 0, name = "default", weights = c(1,1,1,1,1,1,1,1)){
+    if(year != 0) {year_for_comparison <- year}
+  df_1D <- find_slopes_with_one_country(year = year_for_comparison, weight_population = weights[1], weight_GDPpc = weights[2], weight_inflation = weights[3], weight_agriculture = weights[4], weight_industry = weights[5], weight_manufacturing = weights[6], weight_total_energy_supply = weights[7], weight_verified_emisions = weights[8])$data
+  lm <- find_slopes_with_one_country(year = year_for_comparison, weight_population = weights[1], weight_GDPpc = weights[2], weight_inflation = weights[3], weight_agriculture = weights[4], weight_industry = weights[5], weight_manufacturing = weights[6], weight_total_energy_supply = weights[7], weight_verified_emisions = weights[8])$linear
+  print(df_1D)
+  # Create png with the regression line
+  #png(paste("Newscatterplot_with_regression_line_",year_for_comparison,"_with_all_data_and_log=", will_use_log, ".png") , width = 1000, height = 1000)
+  png(paste(name,".png",sep=""), width = 1000, height = 1000)
+  #plot(df_1D$"df_distance", df_1D$"d f_free_distance", xlab = "Combined calculated distance", ylab = "Free distance", main = paste( "Scatterplot of calculated distance and actual distance for the year ", year_for_comparison, sep = ""))  # Color red the points of the scatterpolit where df_1D[3,] contains "Germany"
+  plot(df_1D$"df_distance", df_1D$"df_free_distance", xlab = "Combined calculated distance", ylab = "Free distance")
+ 
+  abline(lm, col = "red")
+  dev.off()
+}
